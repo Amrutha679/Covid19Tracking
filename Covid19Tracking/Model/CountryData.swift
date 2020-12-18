36eeb0cb3738c9500a761bf8e1df36017ff33a9c
Amrutha679 <amruthaedu665@gmail.com>
2020-12-18 11:44:22 +0530

    Binding CountryData

---
+++ Covid19Tracking/Model/CountryData.swift
@@ -0,0 +1,9 @@
+//
+//  CountryData.swift
+//  Covid19Tracking
+//
+//  Created by amrutha on 17/12/20.
+//  Copyright © 2020 amrutha. All rights reserved.
+//
+
+import Foundation
---
+++ Covid19Tracking/Model/CovidData.swift
@@ -0,0 +1,9 @@
+//
+//  CovidData.swift
+//  Covid19Tracking
+//
+//  Created by amrutha on 17/12/20.
+//  Copyright © 2020 amrutha. All rights reserved.
+//
+
+import Foundation
